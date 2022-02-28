07087da184df898d9c3e7ad597a6e47230e50f8d
Diego Rodrigues Barbosa <dbarbosa@clubecirculo.com>
2022-02-28 17:32:31 -0300

    Buttons functions created

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from 'polished'
+import { darken, transparentize } from 'polished'
 
 export const Container = styled.form`
      h2{
@@ -55,12 +55,24 @@ export const TransactionTypeContainer = styled.div`
     grid-template-columns: 1fr 1fr;
     gap: 0.5rem; 
 
-    button {
-        height: 4rem;
+`
+
+interface RadioBoxProps {
+    isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e40',
+}
+
+export const RadioBox = styled.button<RadioBoxProps>`
+      height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
 
-        background: transparent;
+        background: ${(props) => props.isActive ? transparentize(0.8, colors[props.activeColor]) : 'transparent'};
 
         display: flex;
         align-items: center;
@@ -83,8 +95,4 @@ export const TransactionTypeContainer = styled.div`
             font-size: 1rem;
             color: var(--text-title);
         }
-    }
-
-
-
 `
